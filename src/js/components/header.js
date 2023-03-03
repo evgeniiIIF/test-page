66ba41f36c64422580bee7f67bc40cdfd4b2c963
pro100 <pro100.sto@yandex.ru>
2023-03-03 15:45:24 +0300

    done

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,22 +1,29 @@
-import helpers from '../helpers';
+import helpers from "../helpers";
 
 function openMenu() {
 	return new Promise((resolve) => {
-		$('.js-burger').addClass('is-disabled').attr('disabled', true);
+		$(".js-burger").addClass("is-disabled").attr("disabled", true);
 
-		helpers.lockScroll(true, helpers.$header.find('.header__menu'), 'header');
+		helpers.lockScroll(
+			true,
+			helpers.$header.find(".header__menu"),
+			"header"
+		);
 
-		helpers.$header.addClass('is-menu-opened');
-		$('.header__menu').removeClass('is-hidden');
+		helpers.$header.addClass("is-menu-opened");
+		$(".header__menu").removeClass("is-hidden");
 
 		setImmediate(() => {
-			helpers.$body.css('padding-right', `${helpers.getScrollbarWidth()}px`);
-			helpers.$header.css('right', `${helpers.getScrollbarWidth()}px`);
+			helpers.$body.css(
+				"padding-right",
+				`${helpers.getScrollbarWidth()}px`
+			);
+			helpers.$header.css("right", `${helpers.getScrollbarWidth()}px`);
 		});
 
 		setTimeout(() => {
-			$('.header__menu').addClass('is-active');
-			$('.js-burger').removeClass('is-disabled').attr('disabled', false);
+			$(".header__menu").addClass("is-active");
+			$(".js-burger").removeClass("is-disabled").attr("disabled", false);
 
 			resolve();
 		}, 100);
@@ -25,19 +32,23 @@ function openMenu() {
 
 function closeMenu() {
 	return new Promise((resolve) => {
-		$('.js-burger').addClass('is-disabled').attr('disabled', true);
+		$(".js-burger").addClass("is-disabled").attr("disabled", true);
 
-		helpers.lockScroll(false, helpers.$header.find('.header__menu'), 'header');
-		helpers.$body.css('padding-right', '');
-		helpers.$header.css('right', '');
+		helpers.lockScroll(
+			false,
+			helpers.$header.find(".header__menu"),
+			"header"
+		);
+		helpers.$body.css("padding-right", "");
+		helpers.$header.css("right", "");
 
-		helpers.$header.removeClass('is-menu-opened');
+		helpers.$header.removeClass("is-menu-opened");
 
-		$('.header__menu').removeClass('is-active');
+		$(".header__menu").removeClass("is-active");
 
 		setTimeout(() => {
-			$('.header__menu').addClass('is-hidden');
-			$('.js-burger').removeClass('is-disabled').attr('disabled', false);
+			$(".header__menu").addClass("is-hidden");
+			$(".js-burger").removeClass("is-disabled").attr("disabled", false);
 
 			resolve();
 		}, 500);
@@ -48,40 +59,47 @@ function toggleMenu(event) {
 	event.preventDefault();
 	event.stopPropagation();
 
-	if ($(event.currentTarget).hasClass('is-active')) {
-		$(event.currentTarget).removeClass('is-active');
+	if ($(event.currentTarget).hasClass("is-active")) {
+		$(event.currentTarget).removeClass("is-active");
 		closeMenu();
 	} else {
-		$(event.currentTarget).addClass('is-active');
+		$(event.currentTarget).addClass("is-active");
 		openMenu();
 	}
 }
 
 function init() {
-	helpers.$header = $('.header');
+	helpers.$header = $(".header");
 
-	$('.js-burger').on('click.header', toggleMenu);
+	$(".js-burger").on("click.header", toggleMenu);
 
 	helpers.$document
-		.on('click.header', (e) => {
-			let $container = $('.header__menu');
+		.on("click.header", (e) => {
+			let $container = $(".header__menu");
 
-			if ($container.is(e.target) && $container.has(e.target).length === 0 && $container.hasClass('is-active')) {
+			if (
+				$container.is(e.target) &&
+				$container.has(e.target).length === 0 &&
+				$container.hasClass("is-active")
+			) {
 				closeMenu();
-				$('.js-burger').removeClass('is-active');
+				$(".js-burger").removeClass("is-active");
 			}
 		})
-		.on('keyup.header', (e) => {
-			if ((e.key === 'Escape' || e.key === 'Esc') && $('.header__menu').hasClass('is-active')) {
+		.on("keyup.header", (e) => {
+			if (
+				(e.key === "Escape" || e.key === "Esc") &&
+				$(".header__menu").hasClass("is-active")
+			) {
 				closeMenu();
-				$('.js-burger').removeClass('is-active');
+				$(".js-burger").removeClass("is-active");
 			}
 		});
 }
 
 function destroy() {
-	$('.js-burger').off('.header');
-	helpers.$document.off('.header');
+	$(".js-burger").off(".header");
+	helpers.$document.off(".header");
 }
 
 export default {
@@ -90,3 +108,16 @@ export default {
 	openMenu,
 	closeMenu,
 };
+// my-code
+
+let headerLinks = document.querySelectorAll(".header__link");
+headerLinks.forEach((item, index, links) => {
+	item.addEventListener("click", () => {
+		links.forEach((link) => {
+			if (link.classList.contains("header__link--active")) {
+				link.classList.remove("header__link--active");
+			}
+		});
+		item.classList.add("header__link--active");
+	});
+});
